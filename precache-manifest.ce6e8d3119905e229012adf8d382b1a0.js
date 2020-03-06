d5a1d75dd297281127e0eb682b8c6905b1a19a86
rizkisaputra27 <rizkisaputramuhammad1@gmail.com>
2020-03-06 16:14:39 +0700

    deploy

---
+++ precache-manifest.ce6e8d3119905e229012adf8d382b1a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e148abbb6ba759433ce5ebd7fd43de9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aa2fa171a482c5c2439",
+    "url": "/static/css/2.adee7a8c.chunk.css"
+  },
+  {
+    "revision": "0fd521744a8e459d4964",
+    "url": "/static/css/main.ebb4725c.chunk.css"
+  },
+  {
+    "revision": "1aa2fa171a482c5c2439",
+    "url": "/static/js/2.6251da26.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.6251da26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd521744a8e459d4964",
+    "url": "/static/js/main.bc883174.chunk.js"
+  },
+  {
+    "revision": "2fc8e7369f6406e5984d",
+    "url": "/static/js/runtime-main.447fd809.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
